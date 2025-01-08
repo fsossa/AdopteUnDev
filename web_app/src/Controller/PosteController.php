8d4e45497de088acf4aa0629041e01e34cd123ee
Monel Cocou GAFFAN <monelcocou@gmail.com>
2025-01-08 16:10:00 +0100

    feature: Génération du crud de Poste

---
+++ web_app/src/Controller/PosteController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Poste;
+use App\Form\PosteType;
+use App\Repository\PosteRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/poste')]
+final class PosteController extends AbstractController
+{
+    #[Route(name: 'app_poste_index', methods: ['GET'])]
+    public function index(PosteRepository $posteRepository): Response
+    {
+        return $this->render('poste/index.html.twig', [
+            'postes' => $posteRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_poste_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $poste = new Poste();
+        $form = $this->createForm(PosteType::class, $poste);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($poste);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_poste_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('poste/new.html.twig', [
+            'poste' => $poste,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_poste_show', methods: ['GET'])]
+    public function show(Poste $poste): Response
+    {
+        return $this->render('poste/show.html.twig', [
+            'poste' => $poste,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_poste_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Poste $poste, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PosteType::class, $poste);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_poste_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('poste/edit.html.twig', [
+            'poste' => $poste,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_poste_delete', methods: ['POST'])]
+    public function delete(Request $request, Poste $poste, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$poste->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($poste);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_poste_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ web_app/src/Form/PosteType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Company;
+use App\Entity\Poste;
+use App\Entity\Skill;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PosteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('location')
+            ->add('experiences')
+            ->add('min_salary')
+            ->add('max_salary')
+            ->add('created_at', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updated_at', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('company', EntityType::class, [
+                'class' => Company::class,
+                'choice_label' => 'id',
+            ])
+            ->add('skills', EntityType::class, [
+                'class' => Skill::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Poste::class,
+        ]);
+    }
+}
