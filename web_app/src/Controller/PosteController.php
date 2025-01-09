ec69904737b515ee47e3c9d88bb8328f004dbe4e
Fulbert Sossa <fulbert.sossa@etudiant.univ-rennes.fr>
2025-01-09 14:55:31 +0100

    fix bug

--- web_app/src/Controller/PosteController.php
+++ web_app/src/Controller/PosteController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/poste')]
+#[Route('/app/postes')]
 final class PosteController extends AbstractController
 {
     #[Route(name: 'app_poste_index', methods: ['GET'])]
